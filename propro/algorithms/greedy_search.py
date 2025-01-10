ecdb4dad44972d1e17e0377f01ba5f462518dde6
qwertxzy <leo@qwertxzy.me>
2025-01-10 12:00:29 +0100

    Use selection schema parameter from greedy algorithm constructor

--- propro/algorithms/greedy_search.py
+++ propro/algorithms/greedy_search.py
@@ -14,16 +14,22 @@ class GreedySearch(OptimizationAlgorithm):
 
   # TODO: is this generic enough of an interface? How to generify this?
   unplaced_rects: list[Rectangle]
+  selection_schema: SelectionSchema
 
   # Override constructor to remove all initially placed rects
   def __init__(self, problem, selection_schema):
     self.unplaced_rects = []
+    self.selection_schema = selection_schema
+
     for box in problem.current_solution.boxes.values():
       for rect_id in list(box.rects.keys()):
         self.unplaced_rects.append(box.remove_rect(rect_id))
 
     # IDEA: sort rects by size to use big ones first
     self.unplaced_rects.sort(key=lambda r: r.get_area(), reverse=False)
+
+    # Remove now empty boxes from solution
+    problem.current_solution.boxes.clear()
 
     # Now call the base constructor
     super().__init__(problem)
@@ -33,8 +39,7 @@ class GreedySearch(OptimizationAlgorithm):
     if len(self.unplaced_rects) == 0:
       return
 
-    # TODO: make this dynamic like neighborhoods
-    selection_method = SelectionSchema.FIRST_FIT.get_selection_schema()
+    selection_method = self.selection_schema.get_selection_schema()
     # Pop a rect from the list and get the best move
     next_rect = self.unplaced_rects.pop()
     self.problem.current_solution = selection_method(self.problem.current_solution, next_rect)
